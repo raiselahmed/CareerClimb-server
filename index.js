546f703dd9c2ad40c37848371ccb3061c8b8cede
Raisel <raiselahmed985@gmail.com>
2025-06-30 01:30:21 +0600

    update token

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ async function run() {
     // Auth related APIs
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "10d" });
       res
         .cookie("token", token, {
           httpOnly: true,
